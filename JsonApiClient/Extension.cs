15cc431b89b9f65668417b30b8a472dd94d50e4f
Maurits Moeys <maurits.moeys@gmail.com>
2018-12-12 19:47:16 -0800

    feat: usage of standalone client vs fully operation JADNC + client

--- JsonApiClient/Extension.cs
+++ JsonApiClient/Extension.cs
@@ -11,6 +11,7 @@ using JsonApiDotNetCore.Internal;
 using JsonApiDotNetCore.Models;
 using JsonApiDotNetCore.Serialization;
 using JsonApiDotNetCore.Services;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -24,22 +25,43 @@ namespace nmbrs.Extensions.JsonApiClient
     /// </summary>
     public static class AddJsonApiClientExtension
     {
-        public static IServiceCollection AddJsonApiClient(this IServiceCollection services, Action<JsonApiClientOptions> action)
+        public static IServiceCollection AddJsonApiCoreWithClient<TDbContext>(this IServiceCollection services, Action<JsonApiCoreWithClientOptions> action) where TDbContext : DbContext
         {
-            var options = new JsonApiClientOptions();
+            var options = new JsonApiCoreWithClientOptions();
+            var coreOptions = options.Core;
+
+            services.AddJsonApiInternals<TDbContext>(coreOptions);
+            action(options);
+            AddClientInternals(services, options.Client);
+            return services;
+        }
+        public static IServiceCollection AddJsonApiCoreWithClient(this IServiceCollection services, Action<JsonApiCoreWithClientOptions> action)
+        {
+            var options = new JsonApiCoreWithClientOptions();
             var coreOptions = options.Core;
 
             services.AddJsonApiInternals(coreOptions);
             action(options);
+            AddClientInternals(services, options.Client);
+            return services;
+        }
+
+        public static IServiceCollection AddJsonApiClient(this IServiceCollection services, Action<ClientOptions> action)
+        {
+            var options = new ClientOptions();
+            action(options);
+            AddClientInternals(services, options);
+            return services;
+        }
+        private static void AddClientInternals(IServiceCollection services, ClientOptions options)
+        {
             HttpClient httpClient = new HttpClient()
             {
-                BaseAddress = options.Client.BaseAddress
+                BaseAddress = options.BaseAddress
             };
-            ServicePointManager.FindServicePoint(options.Client.BaseAddress).ConnectionLeaseTimeout = 60000;
+            ServicePointManager.FindServicePoint(options.BaseAddress).ConnectionLeaseTimeout = 60000;
             services.AddSingleton(httpClient);
             services.AddScoped<IScopedServiceProvider, JsonApiClientScopedServiceProvider>();
-
-            return services;
         }
     }
 
@@ -49,11 +71,11 @@ namespace nmbrs.Extensions.JsonApiClient
         public int ConnecitonLeaseTimeout { get; set; } = 60000;
 
     }
-    public class JsonApiClientOptions
+    public class JsonApiCoreWithClientOptions
     {
         public JsonApiOptions Core { get; set; }
         public ClientOptions Client { get; set; }
-        public JsonApiClientOptions()
+        public JsonApiCoreWithClientOptions()
         {
             Core = new JsonApiOptions();
             Client = new ClientOptions();
